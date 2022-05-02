f708cf742581bfc80681fa4681e8d6b2de201df9
nol1ck <nolick@icloud.com>
2022-05-02 22:40:16 +0300

    start 2

--- main.py
+++ main.py
@@ -5,6 +5,4 @@ from bs4 import BeautifulSoup as BS
 r = requests.get('https://stopgame.ru/review/new/izumitelno/p1')
 html = BS(r.content, 'html.parser')
 
-for el in html.select('.items > .article-summary'):
-    title = el.select('.caption > a')
-    print(title[0].text)
+
