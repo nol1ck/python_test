4936d9c8f51927df629683cd65a6b15db90fa472
nol1ck <nolick@icloud.com>
2022-05-03 13:07:33 +0300

    first list operation

--- arr.py
+++ arr.py
@@ -1,3 +1,8 @@
-arr = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+first_List = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
-print([item for item in arr if item < 5])
+second_List = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+
+result = list(filter(lambda elem: elem in second_List, first_List))
+
+
