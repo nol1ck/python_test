80ca2d861f25203aae84a5ec4f81f4b30b7ff009
nol1ck <nolick@icloud.com>
2022-05-03 23:28:51 +0300

    binance destructor JSON to Array functional style

--- arr.py
+++ arr.py
@@ -1,23 +1,20 @@
 arr = [{'symbol': 'BNBBUSD', 'price': '153.63000000'}, {'symbol': 'BTCBUSD', 'price': '37800.00000000'},
        {'symbol': 'ETHBUSD', 'price': '2879.24000000'}, {'symbol': 'LTCBUSD', 'price': '100.10000000'}]
 
-keys = list()
 
-for item in arr:
-    keys.append(item['symbol'])
+def destructor_json(argument):
+    index = list()
+    content = list()
 
-values = list()
-
-for item in arr:
-    values.append(item['price'])
-
-new_arr = dict(zip(keys, values))
-print(new_arr['BNBBUSD'])
+    for element in argument:
+        index.append(element['symbol'])
+        content.append(element['price'])
+    return index, content
 
 
+def create_list(argument, function):
+    keys, values = function(argument)
+    return dict(zip(keys, values))
 
 
-
-
-
-
+print(create_list(arr, destructor_json))
