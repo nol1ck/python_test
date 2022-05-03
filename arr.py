993a82ba50b9da7fd3ece6078247e98dfc2aeaab
nol1ck <nolick@icloud.com>
2022-05-03 13:13:34 +0300

    third list operation

--- arr.py
+++ arr.py
@@ -3,7 +3,7 @@ first_List = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 second_List = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 
-result = [item for item in first_List if item in second_List]
+result = list(set(first_List) & set(second_List))
 
 print(result)
 
