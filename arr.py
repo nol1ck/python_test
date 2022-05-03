1fa861c06542dc31f3d18dc38af8e5b322771aa1
nol1ck <nolick@icloud.com>
2022-05-03 13:11:51 +0300

    second list operation

--- arr.py
+++ arr.py
@@ -3,7 +3,7 @@ first_List = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 second_List = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 
-result = list(filter(lambda elem: elem in second_List, first_List))
+result = [item for item in first_List if item in second_List]
 
 print(result)
 
