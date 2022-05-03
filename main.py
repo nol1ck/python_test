e1943de42b9459241ad2e2e0d15914efb4329376
nol1ck <nolick@icloud.com>
2022-05-03 09:12:32 +0300

    commit number 3

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ r = requests.get('https://stopgame.ru/review/new/izumitelno/p1')
 html = BS(r.content, 'html.parser')
 
 
+for el in html.select('.items > .article-summary'):
+    title = el.select('.caption > a')
+    print(title[0].text)
