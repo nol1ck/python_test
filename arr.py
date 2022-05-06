2337eba0b1df0ebe6c85785aa28c81221eeeaf41
nol1ck <nolick@icloud.com>
2022-05-06 10:27:28 +0300

    generator numbers

--- arr.py
+++ arr.py
@@ -1,4 +1,5 @@
 from Ticker import Ticker
+from table.table import Table as table
 
 arr = [{'symbol': 'BNBBUSD', 'price': '153.63000000'}, {'symbol': 'BTCBUSD', 'price': '37800.00000000'},
        {'symbol': 'ETHBUSD', 'price': '2879.24000000'}, {'symbol': 'LTCBUSD', 'price': '100.10000000'}]
@@ -7,5 +8,8 @@ arr = [{'symbol': 'BNBBUSD', 'price': '153.63000000'}, {'symbol': 'BTCBUSD', 'pr
 ticker = Ticker(arr)
 
 print(ticker.create_list())
+
+function_print = table('work')
+function_print.work()
 
 
---
+++ table/table.py
@@ -0,0 +1,7 @@
+class Table:
+
+    def __init__(self, table):
+        self.table = table
+
+    def work(self):
+        print(self.table)
