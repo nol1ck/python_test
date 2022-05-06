2761cac1a7fcf996a2e31305321dfbdd80adad00
nol1ck <nolick@icloud.com>
2022-05-06 12:50:41 +0300

    numbers 2

--- generator/generator.py
+++ generator/generator.py
@@ -6,4 +6,7 @@ numbers_list = lambda: list(random.randint(1000, 5000) for _ in range(30))
 celeron = [i for i in numbers() if i < 3000]
 core3 = [i for i in numbers() if (3000 < i < 4000)]
 core5 = [i for i in numbers() if i > 4000]
+
+
+
 print(core5)
