bb3e28a4c73a1045b507e3929b88b92a9cab8960
nol1ck <nolick@icloud.com>
2022-05-06 20:33:46 +0300

    numbers last change

--- generator/generator.py
+++ generator/generator.py
@@ -8,5 +8,10 @@ core3 = [i for i in numbers() if (3000 < i < 4000)]
 core5 = [i for i in numbers() if i > 4000]
 
 
+frequency = [1000, 1400, 1700, 2200, 2800, 3200, 3500, 3700, 4000, 4300, 4600, 4800, 5000, 5100]
 
-print(core5)
+duron = [item for item in frequency if item <= 1500]
+sempron = [item for item in frequency if 1500 < item <= 2500]
+athlon = [item for item in frequency if 2500 < item <= 3500]
+ryzen = [item for item in frequency if item > 3500]
+print(ryzen)
